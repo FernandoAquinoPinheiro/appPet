b761b770b1797602631ec444da773655e8c51e7a
FernandoAquinoPinheiro <fernando.pinheiro@etetc.sp.gov.br>
2024-10-18 20:19:47 -0300

    css listar

--- resources/views/homeadm.blade.php
+++ resources/views/homeadm.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <article class="article">
-<h2 class="into">Uma aplicação Web para  cadastramento de Pets </h2>
+<h2 class="into">Uma aplicação Web para  Cadastramento e Gerenciamento de Pets </h2>
 </article>
 
 <figure class="text-center">
